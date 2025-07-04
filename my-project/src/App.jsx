c260871639096b481c3a644b031bda41aed974be
Iftikhar-Jan <ijan709@gmail.com>
2025-07-04 15:35:52 +0500

    done changes

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div className='text-green-900'> Heloo acp 4 month coures
-    <h1>za ye push kom code</h1>
+    <div className="text-green-900">
+      {" "}
+      Heloo acp 4 month coures
+      <h1>za ye push kom code doneeeeeeee</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
