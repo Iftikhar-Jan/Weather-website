15801961462db2edd784b9eaa8a14b74cc87009f
Iftikhar-Jan <ijan709@gmail.com>
2025-07-04 15:24:01 +0500

    chages

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='text-green-900'> Heloo</div>
+    <div className='text-green-900'> Heloo acp 4 month coures</div>
   )
 }
 
