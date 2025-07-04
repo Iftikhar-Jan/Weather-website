b3bbb5ea0e8a1043c3a1cf8b76c0c2b4f6d0ecd3
ibrar9596 <khanib647@gmail.com>
2025-07-04 15:33:47 +0500

    new changes

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='text-green-900'> Heloo acp 4 month coures</div>
+    <div className='text-green-900'> Heloo acp 4 month coures
+    <h1>za ye push kom code</h1>
+    </div>
   )
 }
 
